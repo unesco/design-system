0e2f71da07d0cf1b8070f283a8d4f092e45e88c4
branka_andreja <branka.andreja@gmail.com>
2021-02-04 15:39:14 +0100

    Refs #56170 header submenus

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -7,4 +7,18 @@
 
 // Add your custom scripts here, using Drupal.behaviors
 
+Drupal.behaviors.reportMenu = {
+  attach: function (context, settings) {
+    let menuCarousel = $('header .report .menu-level-1');
+
+    menuCarousel.slick({
+      speed: 300,
+      slidesToShow: 1,
+      variableWidth: true,
+      arrows: false,
+      infinite: false,
+    });
+  }
+};
+
 })(jQuery);
