ffe18903aa91d89539ba11cb2d51b53e86b55c29
Anthony Fessy <anthony.fessy@axess.fr>
2021-04-26 15:03:38 +0200

    ref #57563 type parallax on story

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -598,5 +598,24 @@
     }
   };
 
+  Drupal.behaviors.imageParallax = {
+    attach: function (context, settings) {
+
+      $(window).scroll(function(){
+        $('.story-item').each(function(){
+          if ($(this).offset().top < $(window).scrollTop()) {
+            var difference = $(window).scrollTop() - $(this).offset().top;
+            var half = (difference / 2) + 'px';
+            var transform = 'translate3d( 0, ' + half + ',0)';
+
+            $(this).find('img').css('transform', transform);
+          } else {
+            $(this).find('img').css('transform', 'translate3d(0,0,0)');
+          }
+        });
+      });
+
+    }
+  };
 })
 (jQuery);
